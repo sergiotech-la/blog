190b7af28689c973acf839362b2e32039beab19a
Sergio Herrera <sa.herrerasaavedra@gmail.com>
2023-08-06 14:44:22 -0500

    Add delete button to admin view of posts

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get "/blog_posts/:id/edit", to: "blog_posts#edit", as: :edit_blog_post
   get "/blog_posts/:id", to: "blog_posts#show", as: :blog_post
   patch "/blog_posts/:id", to: "blog_posts#update"
+  delete "/blog_posts/:id", to: "blog_posts#destroy"
   post "/blog_posts", to: "blog_posts#create", as: :blog_posts
   # Defines the root path route ("/")
   root "blog_posts#index"
